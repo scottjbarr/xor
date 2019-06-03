7578a2e631eaabae546fd1142ff85404c7f0fcb2
Scott Barr <scottjbarr@gmail.com>
2019-06-03 19:50:44 +1000

    Comments.

--- xor.go
+++ xor.go
@@ -2,8 +2,8 @@ package xor
 
 // EncryptDecrypt performs XOR encryption on the input.
 //
-// If the input is plaintext, the result is an encrypted payload. If the input
-// is encrypted, the plaintext is returned.
+// If the input is plaintext, the result is an encrypted payload. If the
+// input is encrypted, the plaintext is returned.
 //
 // The key can be any length, with larger keys obviously being preferred.
 func EncryptDecrypt(input, key []byte) []byte {
